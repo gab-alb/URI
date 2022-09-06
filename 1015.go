9388d9a2fe6770742c0429ca94c75ed8a1eb1490
Gabriel de Albuquerque <gabriel.lima@digitalsys.com.br>
2022-09-06 01:46:39 -0300

    1015

--- 1015.go
+++ 1015.go
@@ -9,11 +9,14 @@ func main() {
 
 	var x1, y1, x2, y2, distancia float64
 
-	fmt.Printf("Digite os valores de X1 e Y1:2")
-	fmt.Scanf("%f%f", &x1, &y1)
-
-	fmt.Printf("Digite os valores de X2 e Y2:")
-	fmt.Scanf("%f%f", &x2, y2)
+	fmt.Printf("Digite os valores de X1:")
+	fmt.Scanf("%f", &x1)
+	fmt.Printf("Digite os valores de Y1:")
+	fmt.Scanf("%f", &y1)
+	fmt.Printf("Digite os valores de X2:")
+	fmt.Scanf("%f", &x2)
+	fmt.Printf("Digite os valores de Y2:")
+	fmt.Scanf("%f", &y2)
 
 	distancia = math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)))
 
